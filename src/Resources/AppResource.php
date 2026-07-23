8fb1bb9ea8a3c7f6e1f7a65aa58d3e52e1252e24
Ashok <ashokbaruaakas@gmail.com>
2026-07-24 00:19:16 +0600

    feat: add resource base classes and FlexibleJsonResource trait

---
+++ src/Resources/AppResource.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SoftPulze\LaravibeStandards\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Override;
+use SoftPulze\LaravibeStandards\Resources\Concerns\FlexibleJsonResource;
+
+abstract class AppResource extends JsonResource
+{
+    use FlexibleJsonResource;
+
+    /**
+     * Resolve the resource to a plain array for Inertia props.
+     *
+     * @return array<int|string, mixed>
+     */
+    final public function toInertia(): array
+    {
+        return $this->resolve();
+    }
+
+    /**
+     * Create a new resource collection instance.
+     */
+    #[Override]
+    protected static function newCollection($resource): AppResourceCollection
+    {
+        return new AppResourceCollection($resource, static::class);
+    }
+}
